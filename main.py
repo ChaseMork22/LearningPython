55e38368e0004ea6aed7212d327db9b97b66fcfc
ChaseMork22 <817251@mystma.org>
2021-03-22 14:55:35 +0000

    stinkyburger

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 adOne = "stinky "
 adTwo = "cheesy "
-adThree  = "crabby"
+adThree  = "crabby "
 nOne = "hamburger"
-nTwo = "hands"
-nThree = "feet toes"
-
-print("One time there was a " + adOne + adTwo + nOne ". The" + nOne "had " + adThree + nTwo "and " + adThree + nThree ". The" + nTwo
+nTwo = "hands "
+nThree = "feet-toes"
+nFour = "tabel"
+vOne = "run"
+avOne = "quickly "
+vTwo = "fly"
+print(" ")
+print("One time there was a " + adOne + adTwo + nOne + ". The " + nOne + " had " + adThree + nTwo + "and " + adThree + nThree + ". The " + nOne + " could " + avOne + vOne + " accros any " + nFour + " of any size. The " + nOne + " also had the ability to " + vTwo + ".")
+print(" ")
