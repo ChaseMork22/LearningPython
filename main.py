d7e0ebdbf70868767b207d8a908b9bfa04abee96
Mork Chase <817251@stma.k12.mn.us>
2021-03-26 10:22:37 -0500

    stinklight

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-def addTwoNumbers(x, y):
-    return x + y
-print (addTwoNumbers(18, 1))
+light = input("What is color of stoplight?\n")
+
+if light == "green":
+    print("Green means good to eat. GO")
+
+elif light == "yellow":
+    print("sloth")
+
+elif light == "blue":
+    print("Call your mom")
+
+else:
+    print("El Stopo")
